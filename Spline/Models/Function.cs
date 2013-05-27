baad0557525c8617035569260f8f91a3b47b3481
kruttaras <krut.taras@gmail.com>
2013-05-28 00:49:15 +0300

    cleaned2

---
+++ Spline/Models/Function.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Spline.Models
+{
+    class Function
+    {
+        private IList<Func<double, double>> FunctionAndDerivetives;
+
+        private double[] Coefitients;
+    }
+}
---
+++ Spline/Models/Section.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Spline.Models
+{
+    //section of aproximation 
+    class Section
+    {
+        private double LeftPoint;
+        private double RightPoint;
+        private double Mu;
+
+        public void SetRightMarginToHalfOfLine()
+        {
+            RightPoint=(LeftPoint+RightPoint)/2;
+        }
+
+        private Section()
+        {
+        }
+        public Section(double LPoint,double RPoint)
+        {
+            if(LPoint<RPoint)
+            {
+            this.RightPoint=RPoint;
+            this.LeftPoint=LPoint;
+            }else
+            {
+                throw new Exception();
+            }
+        }
+
+}
